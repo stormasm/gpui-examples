2c13fd30ecec3eebbd09a9d599d45abdafec2816
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-02-25 11:12:39 -0800

    trying to get two of these going

--- tailwind/examples/ex00.rs
+++ tailwind/examples/ex00.rs
@@ -18,6 +18,17 @@ impl Render for HelloWorld {
             .text_xl()
             .text_color(rgb(0xffffff))
             .child(format!("A {}", &self.text))
+            .flex()
+            .bg(rgb(0x2e7d32))
+            .size(Length::Definite(Pixels(100.0).into()))
+            .justify_center()
+            .items_center()
+            .shadow_lg()
+            .border()
+            .border_color(rgb(0x0000ff))
+            .text_xl()
+            .text_color(rgb(0xffffff))
+            .child(format!("A {}", &self.text))
     }
 }
 
